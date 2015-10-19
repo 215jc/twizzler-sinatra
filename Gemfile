1a5c4ee7cf92df8dc9e7482e15ff5f53a1f7f0ab
Jason Curran <jc@jasoncurran.com>
2015-10-19 06:45:12 -0400

    heroku setup

--- Gemfile
+++ Gemfile
@@ -10,7 +10,6 @@ gem 'sinatra-flash'
 group :development do
 	gem 'sqlite3'
 end
-
 group :production do
 	gem 'pg'
 end
--- environments.rb
+++ environments.rb
@@ -4,6 +4,11 @@ configure :development do
 end
 
 configure :production do
+	#ActiveRecord::Base.establish_connection('postgres://ENV['DB_USER']:ENV['DB_PASS']@ENV['DB_HOST']:5432/ENV['DB_NAME']')
+	ActiveRecord::Base.establish_connection('postgres://fnkzpuquliqbsf:OIy1CKg74Vl8pJhgUuhKw9l5On@ec2-54-204-15-48.compute-1.amazonaws.com:5432/ddd4prcrti1lul')
+	set :show_exceptions, true
+end
+configure :production do
 	ActiveRecord::Base.establish_connection('postgres://ENV['DB_USER']:ENV['DB_PASS']@ENV['DB_HOST']:5432/ENV['DB_NAME']')
 	set :show_exceptions, true
 end
