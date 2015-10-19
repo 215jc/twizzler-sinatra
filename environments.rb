22f6d8e62af01eda7bf06cd3d812ce977c6dee64
Jason Curran <jc@jasoncurran.com>
2015-10-19 06:30:12 -0400

    heroku setup

--- environments.rb
+++ environments.rb
@@ -1,9 +1,9 @@
 configure :development do
-set :database, "sqlite3:database.sqlite3"
- set :show_exceptions, true
+	set :database, "sqlite3:database.sqlite3"
+	set :show_exceptions, true
 end
 
 configure :production do
- ActiveRecord::Base.establish_connection('postgres://ENV['DB_USER']:ENV['DB_PASS']@ENV['DB_HOST']:5432/ENV['DB_NAME']')
-  set :show_exceptions, true
+	ActiveRecord::Base.establish_connection('postgres://ENV['DB_USER']:ENV['DB_PASS']@ENV['DB_HOST']:5432/ENV['DB_NAME']')
+	set :show_exceptions, true
 end
