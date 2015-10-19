df19f78dc41dff89c2f4d6a0688659bfebd2a049
Jason Curran <jc@jasoncurran.com>
2015-10-19 06:53:23 -0400

    heroku setup

--- db/migrate/20151019101010_create_users_table.rb
+++ db/migrate/20151019101010_create_users_table.rb
@@ -1,6 +1,6 @@
 class CreateUsersTable < ActiveRecord::Migration
 	def change
-		create_table :user_groups do |t|
+		create_table :users do |t|
 			t.string :username
 			t.string :fname
 			t.string :lname
